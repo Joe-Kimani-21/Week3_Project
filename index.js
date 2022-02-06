41478509d6dc3b9b0b8f5267f7fb0ee62498e767
Joseph Kimani <joseph.kimani@student.moringaschool.com>
2022-02-06 15:50:12 +0300

    Add js code format

--- index.js
+++ index.js
@@ -5,30 +5,7 @@ function tabulateAnswers() {
   var c1score = 0;
   var c2score = 0;
   var c3score = 0;
-  var c4score = 0;
-  
-  // get a list of the radio inputs on the page
-  var choices = document.getElementsByTagName('input');
-  // loop through all the radio inputs
-  for (i=0; i<choices.length; i++) {
-    // if the radio is checked..
-    if (choices[i].checked) {
-      // add 1 to that choice's score
-      if (choices[i].value == 'c1') {
-        c1score = c1score + 1;
-      }
-      if (choices[i].value == 'c2') {
-        c2score = c2score + 1;
-      }
-      if (choices[i].value == 'c3') {
-        c3score = c3score + 1;
-      }
-      if (choices[i].value == 'c4') {
-        c4score = c4score + 1;
-      }
-      // If you add more choices and outcomes, you must add another if statement below.
-    }
-  }
+  var c4score = 0
   
   // Find out which choice got the highest score.
   // If you add more choices and outcomes, you must add the variable here.
